fab825bda21f40eb2f44c5d9ab0263e918c07acd
John Hixson <johnhixson2020@u.northwestern.edu>
2019-12-08 16:42:16 -0600

    Done

--- D3_data_journalism/assests/js/app.js
+++ D3_data_journalism/assests/js/app.js
@@ -23,8 +23,11 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
+
 // Import Data
-d3.csv("/assets/data/data.csv").then(function(healthData) {
+d3.csv("/assets/data/data.csv", function(err, healthData) {
+  if (err) throw err;
+console.log(healthData)
 
     // Step 1: Parse Data/Cast as numbers
     // ==============================
@@ -48,19 +51,19 @@ d3.csv("/assets/data/data.csv").then(function(healthData) {
     var yMin;
     var yMax;
 
-    xMin = d3.min(healthData, function (data){
+    xMin = d3.min(healthData, function (data) {
        return data.healthcare;
     });
  
-    xMax = d3.max(healthData, function (data){
+    xMax = d3.max(healthData, function (data) {
       return data.healthcare;
     });
 
-    yMin = d3.min(healthData, function (data){
+    yMin = d3.min(healthData, function (data) {
       return data.poverty;
     });
 
-    yMax = d3.max(healthData, function (data){
+    yMax = d3.max(healthData, function (data) {
       return data.poverty;
    });
 
@@ -85,7 +88,7 @@ d3.csv("/assets/data/data.csv").then(function(healthData) {
     .enter()
     .append("circle")
     .attr("cx", d => xLinearScale(d.healthcare +1.5))
-    .attr("cy", d => yLinearScale(d.poverty +.03))
+    .attr("cy", d => yLinearScale(d.poverty +0.3))
     .attr("r", "12")
     .attr("fill", "blue")
     .attr("opacity", ".5")
@@ -110,7 +113,7 @@ d3.csv("/assets/data/data.csv").then(function(healthData) {
     // Step 8: Create event listeners to display and hide the tooltip
     // ==============================
     circlesGroup.on("click", function(data) {
-      toolTip.show(data, this);
+      toolTip.show(data);
     })
       // onmouseout event
       .on("mouseout", function(data, index) {
@@ -127,7 +130,7 @@ d3.csv("/assets/data/data.csv").then(function(healthData) {
         .attr("x", function(data) {
             return xLinearScale(data.healthcare +1.3);
         })
-        .attr("y", functi(Data) {
+        .attr("y", function(Data) {
             return yLinearScale(data.poverty +.1);
         })
         .text(function(data) {
@@ -146,6 +149,4 @@ d3.csv("/assets/data/data.csv").then(function(healthData) {
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
       .text("In Poverty (%)");
-  }).catch(function(error) {
-    console.log(error);
   });
